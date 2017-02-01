f5119089f2a6815671b76a017d1a2367adbd1352
Damian Moźniak <damian.mozniak@o2.pl>
2017-02-02 00:47:41 +0100

    app_js 0.2

--- app_js/app.js
+++ app_js/app.js
@@ -1,4 +1,4 @@
-var express = require('express');
+/*var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
@@ -25,15 +25,52 @@ io.sockets.on('connection', function(socket){
         connections.splice(connections.indexOf(socket), 1);
         console.log('Disconnected: %s sockets connected', connections.length);
     });
+	
+    socket.on('message', function(data) {
+		console.log(data);
+        socket.broadcast.emit('message', data);
+    });
+});*/
+var clients = [ ];
+var oldConnection;
+var express = require('express');
+var app = express();
+app.use(express.static(__dirname + '/'));
 
-    socket.on('drawClick', function(data) {
-        socket.broadcast.emit('draw', {
-            x: data.x,
-            y: data.y,
-			oldX: data.oldX,
-	  		oldY: data.oldY,
-            type: data.type
-        });
-        console.log(data);
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+
+var server = http.createServer(app);
+
+server.listen(process.env.PORT || 3000);
+console.log('Server running......');
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+// create the server
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+// WebSocket server
+wsServer.on('request', function(request) {
+	
+    var connection = request.accept(null, request.origin);
+	var index = clients.push(connection) - 1;
+	console.log(request);
+	//clients[""] = connection;
+    // This is the most important callback for us, we'll handle
+    // all messages from users here.
+    connection.on('message', function(data) {
+        /*if (message.type === 'utf8') {
+            // process WebSocket message
+        }*/
+		for (var i=0; i < clients.length; i++) {
+        	clients[i].send(data.utf8Data);
+        }
+    });
+
+    connection.on('close', function(connection) {
+		clients.splice(index, 1);
     });
 });
--- app_js/js/scripts.js
+++ app_js/js/scripts.js
@@ -2,7 +2,11 @@
 (function() {
   $(document).ready(function () {
 
+	window.WebSocket = window.WebSocket || window.MozWebSocket;
 
+    //var connection = new WebSocket('ws://sr-94933.onmodulus.net');
+	var connection = new WebSocket('ws://localhost:3000');
+	
   var App, dragging;
   var apiData;
   
@@ -15,132 +19,152 @@
     
   $.get(url,function(data){
     apiData = data;
-  }).done(function() {
+  }).done(function(data) {
+		$.each(data,function( index, value) {
+		
+			$('#boardsList').append('<a href="#" class="list-group-item" data-ip="'+value.ipAddress+'">'+value.board.name+'</a>');
+		
+		});
+	  
+	  
+	  $('#makeBoard').click(function(e){
+		e.preventDefault();
+		$.ajax({
+			  type: "POST",
+			  url: url,
+			  // The key needs to match your method's input parameter (case-sensitive).
+			  data: JSON.stringify({"ipAddress": $('#boardIP').val(), "boardName": $('#boardName').val()}),
+			  contentType: "application/json",
+			  dataType: "json",
+			  success: function(data){
+				location.reload();  
+				},
+			  failure: function(errMsg) {
+				alert(errMsg);
+			  }
+		});
+	  });
+	  App.init = function() {
+		App.canvas = document.createElement('canvas');
+		App.canvas.height = 400;
+		//App.canvas.width = 600;
+		App.canvas.width = $('.canvas-container').width();
+		document.querySelector('div.canvas-container').appendChild(App.canvas);
+		App.ctx = App.canvas.getContext("2d");
+		App.ctx.fillStyle = "solid";
+		App.ctx.strokeStyle = "#bada55";
+		App.ctx.lineWidth = 5;
+		App.ctx.lineCap = "round";
+		App.clear = function() {
+		  return App.ctx.clearRect(0, 0, App.canvas.width, App.canvas.height);
+		};
+		App.draw = function(oldX, oldY, x, y, type) {
+		  if (type === "dragstart") {
+			//App.ctx.beginPath();
+			//return App.ctx.moveTo(x, y);
+		  } else if (type === "drag") {
+			App.ctx.beginPath();
+			if(oldCordX == null)
+				App.ctx.moveTo(x, y);
+			else
+				App.ctx.moveTo(oldX, oldY);
+				
+			App.ctx.lineTo(x, y);
+			//oldCordX = x;
+			//oldCordY = y;
+			App.ctx.stroke();
+			App.ctx.closePath();
+		  } else {
+			//return App.ctx.closePath();
+		  }
+		};
+	  };
+		//console.log(apiData);
+	  //App.socket = io.connect(apiData[0].ipAddress);
+	  //App.socket = io.connect('http://localhost:3000');
+	  //App.socket = io.connect("http://sr-94933.onmodulus.net");
+	  
+	  connection.onmessage = function (data) {
+        // try to decode json (I assume that each message from server is json)
+        try {
+			console.log(data.data);
+            var newData = JSON.parse(data.data);
+			return App.draw(newData.oldX, newData.oldY, newData.x, newData.y, newData.type);
+        } catch (e) {
+            
+        }
+        // handle incoming message
+	};
+	
+	  App.init();
+	
+	  $('canvas').on('mousemove', function(e) {
+		var offset, x, y;
+		if (dragging) {
+		  offset = $(this).offset();
+		  e.offsetX = e.pageX - offset.left;
+		  e.offsetY = e.pageY - offset.top;
+		  x = e.offsetX;
+		  y = e.offsetY;
+		  App.draw(oldCordX, oldCordY, x, y, 'drag');
+		  var newJSON = {
+			x: x,
+			y: y,
+			oldX: oldCordX,
+			oldY: oldCordY,
+			type: 'drag'
+		  }
+		  connection.send(JSON.stringify(newJSON));
+		  oldCordX = x;
+		  oldCordY = y;
+		}
+	  });
+	
+	  $('canvas').on('mousedown', function(e) {
+		var offset, x, y;
+		offset = $(this).offset();
+		e.offsetX = e.pageX - offset.left;
+		e.offsetY = e.pageY - offset.top;
+		x = e.offsetX;
+		y = e.offsetY;
+		//App.draw(x, y, 'dragstart');
+		//App.socket.emit('drawClick', {
+		 // x: x,
+		 // y: y,
+		 // oldX: oldCordX,
+		 // oldY: oldCordY,
+		 // type: 'dragstart'
+		//});
+		oldCordX = x;
+		oldCordY = y;
+		return dragging = true;
+	  });
+		
+	  $('canvas').on('mouseup', function(e) {
+		var offset, x, y;
+		offset = $(this).offset();
+		e.offsetX = e.pageX - offset.left;
+		e.offsetY = e.pageY - offset.top;
+		x = e.offsetX;
+		y = e.offsetY;
+		//App.draw(x, y, 'dragend');
+		//App.socket.emit('drawClick', {
+		//  x: x,
+		//  y: y,
+		//  oldX: oldCordX,
+		 // oldY: oldCordY,
+		//  type: 'dragend'
+		//});
+		return dragging = false;
+	  });
+	
+	  $('#clear').on('click', function(e) {
+		return App.clear();
+	  });
+	  });
+   
 
-  
-  $('#makeBoard').click(function(e){
-    e.preventDefault();
-    $.ajax({
-          type: "POST",
-          url: url,
-          // The key needs to match your method's input parameter (case-sensitive).
-          data: JSON.stringify({"ipAddress": "http://localhost:3000", "boardName":"Ravi"}),
-          contentType: "application/json",
-          dataType: "json",
-          success: function(data){alert(data);},
-          failure: function(errMsg) {
-            alert(errMsg);
-          }
-	});
-  });
-  App.init = function() {
-    App.canvas = document.createElement('canvas');
-    App.canvas.height = 400;
-    //App.canvas.width = 600;
-    App.canvas.width = $('.canvas-container').width();
-    document.querySelector('div.canvas-container').appendChild(App.canvas);
-    App.ctx = App.canvas.getContext("2d");
-    App.ctx.fillStyle = "solid";
-    App.ctx.strokeStyle = "#bada55";
-    App.ctx.lineWidth = 5;
-    App.ctx.lineCap = "round";
-    App.clear = function() {
-      return App.ctx.clearRect(0, 0, App.canvas.width, App.canvas.height);
-    };
-    App.draw = function(oldX, oldY, x, y, type) {
-      if (type === "dragstart") {
-        //App.ctx.beginPath();
-        //return App.ctx.moveTo(x, y);
-      } else if (type === "drag") {
-		App.ctx.beginPath();
-		if(oldCordX == null)
-        	App.ctx.moveTo(x, y);
-		else
-			App.ctx.moveTo(oldX, oldY);
-			
-        App.ctx.lineTo(x, y);
-		//oldCordX = x;
-		//oldCordY = y;
-        App.ctx.stroke();
-		App.ctx.closePath();
-      } else {
-        //return App.ctx.closePath();
-      }
-    };
-  };
-	console.log(apiData[0].ipAddress);
-  App.socket = io.connect('http://sr-94933.onmodulus.net');
-  //App.socket = io.connect('http://localhost:3000');
-
-  App.socket.on('draw', function(data) {
-    return App.draw(data.oldX, data.oldY, data.x, data.y, data.type);
-  });
-
-  App.init();
-
-  $('canvas').on('mousemove', function(e) {
-    var offset, x, y;
-    if (dragging) {
-      offset = $(this).offset();
-      e.offsetX = e.pageX - offset.left;
-      e.offsetY = e.pageY - offset.top;
-      x = e.offsetX;
-      y = e.offsetY;
-      App.draw(oldCordX, oldCordY, x, y, 'drag');
-      App.socket.emit('drawClick', {
-        x: x,
-        y: y,
-		oldX: oldCordX,
-	  	oldY: oldCordY,
-        type: 'drag'
-      });
-	  oldCordX = x;
-	  oldCordY = y;
-    }
-  });
-
-  $('canvas').on('mousedown', function(e) {
-    var offset, x, y;
-    offset = $(this).offset();
-    e.offsetX = e.pageX - offset.left;
-    e.offsetY = e.pageY - offset.top;
-    x = e.offsetX;
-    y = e.offsetY;
-    //App.draw(x, y, 'dragstart');
-    /*App.socket.emit('drawClick', {
-      x: x,
-      y: y,
-	  oldX: oldCordX,
-	  oldY: oldCordY,
-      type: 'dragstart'
-    });*/
-	oldCordX = x;
-	oldCordY = y;
-    return dragging = true;
-  });
-
-  $('canvas').on('mouseup', function(e) {
-    var offset, x, y;
-    offset = $(this).offset();
-    e.offsetX = e.pageX - offset.left;
-    e.offsetY = e.pageY - offset.top;
-    x = e.offsetX;
-    y = e.offsetY;
-    /*App.draw(x, y, 'dragend');
-    App.socket.emit('drawClick', {
-      x: x,
-      y: y,
-	  oldX: oldCordX,
-	  oldY: oldCordY,
-      type: 'dragend'
-    });*/
-    return dragging = false;
-  });
-
-  $('#clear').on('click', function(e) {
-    return App.clear();
-  });
-  });
+    
   });
 }).call(this);
 
